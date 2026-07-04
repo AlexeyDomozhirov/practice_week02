04823f04f677d132055c0dfbc1c210a87bdd46ec
Alexei <alexei-2006@bk.ru>
2026-07-04 17:21:58 +0600

    Implementation of eighth task

---
+++ CommandRunner/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Reflection;
+using CommandLib;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length < 2)
+        {
+            Console.WriteLine("Usage: CommandRunner DirectorySize <path>");
+            Console.WriteLine("       CommandRunner FindFiles <path> <mask>");
+            return;
+        }
+
+        var assembly = Assembly.LoadFrom("FileSystemCommands.dll");
+
+        ICommand command = args[0].ToLowerInvariant() switch
+        {
+            "directorysize" => (ICommand)Activator.CreateInstance(
+                assembly.GetType("FileSystemCommands.DirectorySizeCommand"),
+                new object[] { args[1] }),
+
+            "findfiles" => (ICommand)Activator.CreateInstance(
+                assembly.GetType("FileSystemCommands.FindFilesCommand"),
+                new object[] { args[1], args[2] }),
+
+            _ => throw new ArgumentException($"Unknown command: {args[0]}")
+        };
+
+        command.Execute();
+    }
+}
---
+++ FileSystemCommands/Class1.cs
@@ -0,0 +1,74 @@
+﻿namespace FileSystemCommands;
+
+using System.IO;
+using CommandLib;
+
+public class DirectorySizeCommand : ICommand
+{
+    public string dir_path;
+
+    public DirectorySizeCommand(string _dir_path)
+    {
+        dir_path = _dir_path;
+    }
+
+    public void Execute()
+    {
+        if (!Directory.Exists(dir_path))
+        {
+            Console.WriteLine($"Directory not found: {dir_path}");
+            return;
+        }
+
+        Console.WriteLine($"Size of directory '{dir_path}': {CalculateDirectorySize(dir_path)} bytes");
+    }
+
+    private long CalculateDirectorySize(string path)
+    {
+        long total = 0;
+
+        foreach (var file in Directory.GetFiles(path))
+        {
+            total += new FileInfo(file).Length;
+        }
+        foreach (var dir in Directory.GetDirectories(path))
+        {
+            total += CalculateDirectorySize(dir);
+        }
+
+        return total;
+    }
+}
+
+public class FindFilesCommand : ICommand
+{
+    public string dir_path;
+    public string search_pattern;
+
+    public FindFilesCommand(string _directoryPath, string _searchPattern)
+    {
+        dir_path = _directoryPath;
+        search_pattern = _searchPattern;
+    }
+
+    public void Execute()
+    {
+        if (!Directory.Exists(dir_path))
+        {
+            Console.WriteLine($"Directory not found: {dir_path}");
+            return;
+        }
+
+        try
+        {
+            var files = Directory.GetFiles(dir_path, search_pattern, SearchOption.AllDirectories);
+            Console.WriteLine($"Found {files.Length} files matching '{search_pattern}' in '{dir_path}':");
+            foreach (var file in files)
+                Console.WriteLine(file);
+        }
+        catch (IOException ex)
+        {
+            Console.WriteLine($"IO error: {ex.Message}");
+        }
+    }
+}
