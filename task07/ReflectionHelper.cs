efd1539ef3593048ab0b72bcc9d0eec9a65d2bbd
Alexei <alexei-2006@bk.ru>
2026-07-08 17:43:53 +0600

    sync 7 task logic

---
+++ task07/ReflectionHelper.cs
@@ -0,0 +1,46 @@
+﻿using System.Reflection;
+
+namespace task07;
+
+public static class ReflectionHelper
+{
+    public static void PrintTypeInfo(Type type)
+    {
+	if(type == null) return;
+
+        if (Attribute.GetCustomAttribute(type, typeof(DisplayNameAttribute)) is DisplayNameAttribute classDisplay)
+        {
+            Console.WriteLine($"Отображаемое имя: {classDisplay.DisplayName}");
+        }
+
+        if (Attribute.GetCustomAttribute(type, typeof(VersionAttribute)) is VersionAttribute versionAttr)
+        {
+            Console.WriteLine($"Версия: {versionAttr.Major}.{versionAttr.Minor}");
+        }
+
+        Console.WriteLine("--- Свойства ---");
+        var properties = type.GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
+        foreach (var prop in properties)
+        {
+            string propDisplay = Attribute.GetCustomAttribute(prop, typeof(DisplayNameAttribute)) is DisplayNameAttribute pDisplay 
+                ? $" ({pDisplay.DisplayName})" 
+                : string.Empty;
+            
+            Console.WriteLine($"- {prop.Name} {propDisplay}");
+        }
+
+        Console.WriteLine("\n--- Методы ---");
+        var methods = type.GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
+        foreach (var method in methods)
+        {
+            if (method.IsSpecialName) continue;
+
+            string methodDisplay = Attribute.GetCustomAttribute(method, typeof(DisplayNameAttribute)) is DisplayNameAttribute mDisplay 
+                ? $" ({mDisplay.DisplayName})" 
+                : string.Empty;
+
+            Console.WriteLine($"- {method.Name} {methodDisplay}");
+        }
+    }
+}
+
